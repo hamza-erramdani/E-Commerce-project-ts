2a9bca2229d6df46be23796d6f2b68a953b4c443
Hamza ELRa <ramdanihamza0.0@gmail.com>
2025-08-06 21:27:30 +0100

    Product feature

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRoutes from "./routes/userRoutes";
+import productsRoutes from "./routes/productsRoutes";
+import { seedInitialProducts } from "./services/productService";
 
 const app = express();
 const PORT = 3001;
@@ -11,7 +13,11 @@ mongoose
   .then(() => console.log("mongodb connected"))
   .catch((err) => console.log(err));
 
+// seed the products to database
+seedInitialProducts();
+
 app.use("/user", userRoutes);
+app.use("/products", productsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ src/services/productService.ts
@@ -0,0 +1,14 @@
+import productModel from "../models/productModel";
+
+export const getAllProducts = async () => {
+  return await productModel.find();
+};
+export const seedInitialProducts = async () => {
+  const products = [
+    { title: "asus rog strix", image: "https://m.media-amazon.com/images/I/81Cm1VMdxrL._AC_SL1500_.jpg", price: 15000, stock: 5 },
+  ];
+  const existingProducts = await getAllProducts();
+  if (existingProducts.length === 0) {
+    await productModel.insertMany(products);
+  }
+};
